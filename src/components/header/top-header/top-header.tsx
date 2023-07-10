eafab34302c025649753e47828fe7677287d8a8c
AleksandraLiadova <56653235+AleksandraLiadova@users.noreply.github.com>
2023-07-10 16:44:32 +0300

    add theme toggle

--- src/components/header/top-header/top-header.tsx
+++ src/components/header/top-header/top-header.tsx
@@ -3,23 +3,110 @@ import { SkypeSVG, WhatsAppSVG } from './icons';
 import { useTranslation } from "react-i18next"
 import { LanguageSelector } from './language-selector';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import { useState, useEffect , useContext} from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { ThemeContext } from '../../../App';
 
-const TopHeaderBlock = styled.div`
+interface Theme {
+    title?: string;
+    bg?: string;
+}
+
+const TopHeaderBlock = styled.div<{ $theme?: Theme }>`
     height: 48px;
-    background-color:  ${props => props.theme.bg};
+    background-color:  ${props => props.theme?.bg};
     display: flex;
     align-items: center;
     justify-content: end;
 `
 
-const ContactInfo = styled.div<{ $width?: string }>`
+const ContactInfo = styled.div<{ $width?: string, $theme?: Theme }>`
     display: flex;
     padding-right: 25px;
     width: ${props => props.$width || "auto"};
-    color: ${props => props.theme.title};
+    color: ${props => props.theme?.title};
 `
+
+const Theme = styled.div<{ $width?: string, $theme?: Theme }>`
+caret-color: transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-right: 25px;
+    position: relative;
+    height: 100%;
+    color: ${props => props.theme?.title};
+`
+
+const ToggleSwitch = styled.div`
+caret-color: transparent;
+    position: relative;
+    width: 200px;
+    height: 100px;
+    transform: scale(0.3);
+`;
+
+const Label = styled.label`
+caret-color: transparent;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100px;
+    background-color: #28292c;
+    border-radius: 50px;
+    cursor: pointer;
+
+`;
+
+const Slider = styled.span`
+caret-color: transparent;
+    position: absolute;
+    width: 100%;
+    height: 100px;
+    background-color: #28292c;
+    border-radius: 50px;
+    cursor: pointer;
+
+
+    &::before {
+        content: "";
+        position: absolute;
+        top: 13px;
+        left: 16px;
+        width: 75px;
+        height: 75px;
+        border-radius: 50%;
+        box-shadow: inset 28px -4px 0px 0px #d8dbe0;
+        background-color: #28292c;
+        transition: 0.3s;
+    }
+`;
+
+const Input = styled.input`
+    position: absolute;
+    width: 100%;
+    height: 100px;
+    background-color: #28292c;
+    border-radius: 50px;
+    cursor: pointer;
+
+    opacity: 0;
+
+    caret-color: transparent;
+    
+    transform: scale(0.3);
+
+    &:checked ~ ${Slider} {
+        background-color: #d8dbe0;
+    }
+
+    &:checked ~ ${Slider}::before {
+        transform: translateX(95px);
+        background-color: #28292c;
+        box-shadow: none;
+    }
+`;
+
 
 export const TopHeader = () => {
     const { t } = useTranslation();
@@ -34,8 +121,16 @@ export const TopHeader = () => {
             <ContactInfo><SkypeSVG /> <WhatsAppSVG /></ContactInfo>
 
             <ContactInfo><LanguageSelector /></ContactInfo>
-            <ContactInfo><input type="checkbox"  onChange={toggleTheme} checked={theme === 'dark'}></input></ContactInfo>
-
+            <Theme>
+            {t('theme')}
+                <ToggleSwitch>
+                    <Label>
+                        <Input type='checkbox' onChange={toggleTheme} checked={theme === 'light'} />
+                        <Slider className='slider'></Slider>
+                    </Label>
+                </ToggleSwitch>
+            </Theme>
+            {/* <ContactInfo><input type="checkbox"  onChange={toggleTheme} checked={theme === 'dark'}></input></ContactInfo> */}
             <ContactInfo $width="133px">{t('request-call')}</ContactInfo>
         </TopHeaderBlock>
     )
